8cccf4e4bee6f4c03f64332396e9f3e0a82ca6d4
Fede <fedecortes01@gmail.com>
2023-09-17 13:06:41 -0300

    Params para las publicaciones. Testeado

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
   return (
     <>
       <Crear></Crear>
-      <Publicacion></Publicacion>
+      <Publicacion url={"https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg"} desc={"Soy una descripción"}></Publicacion>
 
-      <Publicacion></Publicacion>
+      <Publicacion url="https://img.freepik.com/foto-gratis/perfil-hombre-barbudo-estilo-hizo-nuevo-peinado-peluqueria_176420-18800.jpg?w=2000" desc="Tambien soy una descripción, para el proyecto de instagram"></Publicacion>
 
-      <Publicacion></Publicacion>
+      <Publicacion url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeJ1jMkAd5J5wA9vvBIuia473d5xvaZkC9Nu0SjQumUF2WO9g6mDocVtaEzW8d1R7sX-c&usqp=CAU" desc="Tambien soy una descripción un poco mas larga para ver como queda, para el proyecto de instagram"></Publicacion>
     </>
   )
 }
--- src/components/Publicacion.jsx
+++ src/components/Publicacion.jsx
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { useState } from 'react';
 
-const Publicacion = () => {
+const Publicacion = ({url, desc}) => {
   const [meGusta, setMeGusta] = useState(0);
 
   const agregarMeGusta = () =>{
@@ -19,14 +19,14 @@ const Publicacion = () => {
     
     >
       <img
-        src="https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg"
+        src={url}
         style={{ width: "400px" }}
         alt="Imagen"
       />
 
       <Box
         style={{
-          
+          width:"400px",
           marginTop: "16px",
           padding: "16px",
           display: "flex",
@@ -39,7 +39,7 @@ const Publicacion = () => {
           <p style={{ margin: "0", marginLeft: "8px" }}>{meGusta} Me gusta</p>
         </Box>
         <Typography variant="body2" color="textSecondary">
-          Descripcion de la imagen
+          {desc}
         </Typography>
       </Box>
     </Box>
