8071437de0e6d00d5ea898fd0f1d20792c063065
Fede <fedecortes01@gmail.com>
2023-09-17 12:13:02 -0300

    Funcionalidad de "me gusta" por componente Publ.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ function App() {
   return (
     <>
       <Publicacion></Publicacion>
+
+      <Publicacion></Publicacion>
+      
+      <Publicacion></Publicacion>
     </>
   )
 }
--- src/components/Publicacion.jsx
+++ src/components/Publicacion.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
-
+import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { useState } from 'react';
 
 const Publicacion = () => {
+  const [meGusta, setMeGusta] = useState(0);
+
+  const agregarMeGusta = () =>{
+    setMeGusta(meGusta+1);
+  }
   return (
-    <div>
-      
-        <img src="https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg" style={{width:"400px"}}></img>
-        
-        <Box style={{display:"flex", justifyContent:"space-between"}}>
-        <Box>
-        <FavoriteBorderIcon fontSize="large"></FavoriteBorderIcon>
-        <p style={{marginTop:"0"}}>1 Me gusta</p>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    
+    >
+      <img
+        src="https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg"
+        style={{ width: "400px" }}
+        alt="Imagen"
+      />
+
+      <Box
+        style={{
+          
+          marginTop: "16px",
+          padding: "16px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+        }}
+      >
+        <Box display="flex" alignItems="center">
+          <FavoriteBorderIcon fontSize="large" onClick={agregarMeGusta}/>
+          <p style={{ margin: "0", marginLeft: "8px" }}>{meGusta} Me gusta</p>
         </Box>
         <Typography variant="body2" color="textSecondary">
-       Descripcion de la imagen
+          Descripcion de la imagen
         </Typography>
-        </Box>
-      
-
-    </div>
-  )
+      </Box>
+    </Box>
+  );
 }
 
-export default Publicacion
+export default Publicacion;
