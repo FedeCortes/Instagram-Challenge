2fff54b0172c3eaf20988c4e76789cae5bc42bd9
Fede <fedecortes01@gmail.com>
2023-09-18 20:07:55 -0300

    Eliminando cosas innecesarias

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import Publicacion from './components/Publicacion'
 import Crear from './components/Crear'
-
 import ListarPublicaciones from './components/ListarPublicaciones'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
